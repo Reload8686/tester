5216e462eaf1f317791343cbe7d3c80eaaef6b2b
reload <reload@reload@gm8686@gmail.com>
2022-05-25 11:48:01 +0700

    initial commit

---
+++ env/aloha.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('aloha-home.html')
+@app.route('/about/')
+def about():
+    return render_template('aloha-about.html')
+if __name__ == '__main__':
+    app.run(debug=True)
+
